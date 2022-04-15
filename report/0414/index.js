797d6c9b4cb696261174acffb261ae39e2174c6a
lhw421 <gpdnjs421@gmail.com>
2022-04-15 19:54:10 +0900

    14일 과제 드롭다운 수정

--- report/0414/index.js
+++ report/0414/index.js
@@ -7,9 +7,9 @@ let langSelect = document.getElementById("favoriteLanguageSelect");
 
 let isListVisible = false;
 
-langDiv.onclick = hideList;
-
 langDiv.addEventListener("focusin", function(){
+    dropdownList.style.display = isListVisible ? "none" : "block";
+    isListVisible = !isListVisible;
     this.style.outline = "3px solid #F8E4FF";
     this.style.border = "1px solid #9B51E0";
     this.style.fontSize = "14px";
@@ -24,20 +24,19 @@ langDiv.addEventListener("focusout", function(){
     this.style.lineHeight = "14px";
     this.style.backgroundImage = 'url("./public/icon-Triangle-down.png")';
     dropdownList.style.display = "none";
-    isListVisible = !isListVisible;
+    isListVisible=!isListVisible;
 })
 
-dropdownList.onclick = hideList;
+dropdownList.onclick = function(){
+}
 
 for(let i = 0; i<listItems.length; i++){
-    listItems[i].onclick = function(){
+    listItems[i].onmousedown = function(){
         // 값을 서버로 전송할 때는 필요하다?
         langSelect.options[i+1].setAttribute("selected", "selected");
         langText.innerHTML = listItems[i].innerHTML;
+        dropdownList.style.display = "none";
+        if(isListVisible)
+            !isListVisible;
     }
-}
-
-function hideList(){
-    dropdownList.style.display = isListVisible ? "none" : "block";
-    isListVisible = !isListVisible;
 }
