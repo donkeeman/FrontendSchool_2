18b781e466af9adde8115f2b432cb8fa5d727061
lhw421 <gpdnjs421@gmail.com>
2022-04-19 09:50:13 +0900

    드롭다운 코드 수정 및 삭제

--- report/0414/index.js
+++ report/0414/index.js
@@ -1,15 +1,11 @@
-let langDiv = document.getElementById("favoriteLanguageDiv");
-let langText = document.getElementById("favoriteLanguageText");
+let langButton = document.getElementById("favoriteLanguageButton");
 let dropdownList = document.getElementById("dropdownList");
 let listItems = dropdownList.querySelectorAll("li");
 // select는 보이지는 않지만 리스트의 인덱스와 매칭하기 위해서
 let langSelect = document.getElementById("favoriteLanguageSelect");
 
-let isListVisible = false;
-
-langDiv.addEventListener("focusin", function(){
-    dropdownList.style.display = isListVisible ? "none" : "block";
-    isListVisible = !isListVisible;
+langButton.addEventListener("focusin", function(){
+    dropdownList.style.display = "block";
     this.style.outline = "3px solid #F8E4FF";
     this.style.border = "1px solid #9B51E0";
     this.style.fontSize = "14px";
@@ -17,14 +13,13 @@ langDiv.addEventListener("focusin", function(){
     this.style.backgroundImage = 'url("./public/icon-Triangle-up.png")';
 })
 
-langDiv.addEventListener("focusout", function(){
+langButton.addEventListener("focusout", function(){
+    dropdownList.style.display = "none";
     this.style.outline = "none";
     this.style.border = "1px solid #C4C4C4";
     this.style.fontSize = "12px";
     this.style.lineHeight = "14px";
     this.style.backgroundImage = 'url("./public/icon-Triangle-down.png")';
-    dropdownList.style.display = "none";
-    isListVisible=!isListVisible;
 })
 
 for(let i = 0; i<listItems.length; i++){
@@ -34,9 +29,7 @@ for(let i = 0; i<listItems.length; i++){
             langSelect.options[j].selected = false;
         // 선택한 리스트의 인덱스를 option의 인덱스와 매칭
         langSelect.options[i+1].selected = true;
-        langText.innerHTML = listItems[i].innerHTML;
+        langButton.innerHTML = listItems[i].innerHTML;
         dropdownList.style.display = "none";
-        if(isListVisible)
-            !isListVisible;
     }
 }
