920226d6f0ac95255cfff078746a9ce3c71dc4f5
lhw421 <gpdnjs421@gmail.com>
2022-04-19 20:44:24 +0900

    14일 드롭박스 과제 버튼 포커스 받도록 css, js 수정

--- report/0414/index.js
+++ report/0414/index.js
@@ -4,27 +4,18 @@ let listItems = dropdownList.querySelectorAll("li");
 // select는 보이지는 않지만 리스트의 인덱스와 매칭하기 위해서
 let langSelect = document.getElementById("favoriteLanguageSelect");
 
-let buttonClassName = langButton.className;
-let listClassName = dropdownList.className;
-
-langButton.addEventListener("focusin", function(){
-    langButton.className =  buttonClassName + " on";
-    dropdownList.className = listClassName + " on";
-})
-
-langButton.addEventListener("focusout", function(){
-    langButton.className = buttonClassName;
-    dropdownList.className = listClassName;
+langButton.addEventListener("focusin", () => {
+    dropdownList.classList.toggle("on");
 })
 
 for(let i = 0; i<listItems.length; i++){
-    listItems[i].onmousedown = function(){
+    listItems[i].onclick = function(){
         // selected 초기화
         for(let j = 0; j<langSelect.options.length; j++)
             langSelect.options[j].selected = false;
         // 선택한 리스트의 인덱스를 option의 인덱스와 매칭
         langSelect.options[i+1].selected = true;
         langButton.innerHTML = listItems[i].innerHTML;
-        dropdownList.className = listClassName;
+        dropdownList.classList.remove("on");
     }
 }
