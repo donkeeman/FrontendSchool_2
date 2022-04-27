25b936b3111cd513a3e314442b564213dc6a5a80
lhw421 <gpdnjs421@gmail.com>
2022-04-27 20:11:09 +0900

    27일 실습

---
+++ javascript/002.js
@@ -0,0 +1,4 @@
+document.getElementById("one").innerHTML = "Hello World <strong>document element</strong>";
+console.log("Hello World console");
+window.alert("Hello World alert");
+document.write("Hello World <strong>write</strong>");
