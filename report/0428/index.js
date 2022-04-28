46ff667f59bb68c5cb4b9d1fd619605b8e5bcc9d
lhw421 <gpdnjs421@gmail.com>
2022-04-28 17:28:01 +0900

    28일 과제

--- report/0428/index.js
+++ report/0428/index.js
@@ -5,5 +5,9 @@ randomButton.onclick = () => {
     let start = parseInt(document.getElementById("start").value);
     let end = parseInt(document.getElementById("end").value);
 
+    if(end < start){
+        alert("시작 숫자는 끝 숫자보다 작아야 합니다.");
+        return;
+    }
     result.innerHTML = parseInt(Math.random() * end) + start;
 }
