70664ddf1dceb7c0177bf797d64edfc05ff71158
donkeeman <doonkeeemaan@gmail.com>
2022-05-12 21:00:09 +0900

    js 수정

--- report/0414/index.js
+++ report/0414/index.js
@@ -1,15 +1,15 @@
-let langButton = document.getElementById("favLangButton");
-let dropdownList = document.getElementById("dropdownList");
-let listItems = dropdownList.querySelectorAll("li");
+const langButton = document.querySelector("#favLangButton");
+const dropdownList = document.querySelector("#dropdownList");
+const listItems = dropdownList.querySelectorAll("li");
 // select는 보이지는 않지만 리스트의 인덱스와 매칭하기 위해서
-let langSelect = document.getElementById("favoriteLanguageSelect");
+const langSelect = document.querySelector("#favoriteLanguageSelect");
 
 langButton.addEventListener("focusin", () => {
     dropdownList.classList.toggle("on");
 })
 
 for(let i = 0; i<listItems.length; i++){
-    listItems[i].onclick = function(){
+    listItems[i].addEventListener("click", () => {
         // selected 초기화
         for(let j = 0; j<langSelect.options.length; j++)
             langSelect.options[j].selected = false;
@@ -17,5 +17,5 @@ for(let i = 0; i<listItems.length; i++){
         langSelect.options[i+1].selected = true;
         langButton.innerHTML = listItems[i].innerHTML;
         dropdownList.classList.remove("on");
-    }
+    })
 }
