e915ba5c74927d6a33ec3a09992511a234e8c0b3
donkeeman <doonkeeemaan@gmail.com>
2022-05-14 01:16:59 +0900

    12일 과제 캐러셀 완성

--- report/0512/index.js
+++ report/0512/index.js
@@ -1,13 +1,15 @@
 const prevButton = document.querySelector("#prevButton");
 const nextButton = document.querySelector("#nextButton");
 const carousel = document.querySelector(".carousel");
-const listItems = document.querySelectorAll("Li");
 
 const carouselItems = [
     "./public/001.png",
     "./public/004.png",
     "./public/007.png",
-];
+    "./public/152.png",
+    "./public/155.png",
+    "./public/158.png",
+]
 
 const createItems = (n) => {
     for(let i = 0; i<n; i++){
@@ -15,14 +17,23 @@ const createItems = (n) => {
         let itemImage = document.createElement("Img");
         itemImage.src = carouselItems[i];
         item.appendChild(itemImage);
-        item.style.transform = "translateZ(2rem) rotateY(" + 360/n*(1-i) + "deg)";
+        item.style.transform = "rotateY(" + 360/n*i + "deg) translateZ(300px)";
         carousel.appendChild(item);
     }
 }
 
+const rotateCarousel = (n) => {
+    carousel.style.transform = "translateZ(-300px) rotateY(" + (n)/carouselItems.length*360 + "deg)";
+};
+
+let currentIndex = 0;
 
 window.addEventListener("load", createItems(carouselItems.length));
 
 nextButton.addEventListener("click", () => {
-   
+    rotateCarousel(--currentIndex);
 });
+
+prevButton.addEventListener("click", () => {
+    rotateCarousel(++currentIndex);
+})
