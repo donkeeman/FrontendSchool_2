b686355b06072c6352a727fa2985b97899133720
donkeeman <doonkeeemaan@gmail.com>
2022-05-14 01:42:22 +0900

    12일 과제 이미지에 alt 추가

--- report/0512/index.js
+++ report/0512/index.js
@@ -3,19 +3,35 @@ const nextButton = document.querySelector("#nextButton");
 const carousel = document.querySelector(".carousel");
 
 const carouselItems = [
-    "./public/001.png",
-    "./public/004.png",
-    "./public/007.png",
-    "./public/152.png",
-    "./public/155.png",
-    "./public/158.png",
+    {
+        src: "./public/001.png",
+        alt: "이상해씨",
+    },
+    {
+        src: "./public/004.png",
+        alt: "파이리",
+    },
+    {
+        src: "./public/007.png",
+        alt: "꼬부기",
+    },{
+        src: "./public/152.png",
+        alt: "치코리타",
+    },{
+        src: "./public/155.png",
+        alt: "브케인",
+    },{
+        src: "./public/158.png",
+        alt: "꼬부기",
+    },
 ]
 
 const createItems = (n) => {
     for(let i = 0; i<n; i++){
         let item = document.createElement("Li");
         let itemImage = document.createElement("Img");
-        itemImage.src = carouselItems[i];
+        itemImage.src = carouselItems[i].src;
+        itemImage.alt = carouselItems[i].alt;
         item.appendChild(itemImage);
         item.style.transform = "rotateY(" + 360/n*i + "deg) translateZ(300px)";
         carousel.appendChild(item);
