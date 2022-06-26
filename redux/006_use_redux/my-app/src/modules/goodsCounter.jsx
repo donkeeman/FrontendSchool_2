eb6c3aba1f053b24065d5e29823061a87c4fe147
donkeeman <doonkeeemaan@gmail.com>
2022-06-26 19:01:38 +0900

    20일 실습

---
+++ redux/006_use_redux/my-app/src/modules/goodsCounter.jsx
@@ -0,0 +1,38 @@
+// 액션 생성함수 : 실수를 줄이고, 코드의 양을 줄이기 위해 사용
+export const addNumber = () => {
+    return {type : 'ADD'}
+}
+
+export const substractNumber = () => {
+    return {type : 'SUBSTRACT'}
+}
+
+// 초기값
+const initialState = {
+    stock : 10, // 재고
+    goods : 1    // user의 파우치 구매 개수
+}
+
+const goodsReducer = (state = initialState, action) => {
+    console.log(action)
+    console.log(action.type)
+    console.log(state)
+    switch (action.type) {
+        case 'ADD':
+            return {
+                ...state,
+                stock : state.stock <= 0 ? state.stock = 0 : state.stock - 1,
+                goods : state.goods >= 10 ? state.goods = 10 : state.goods + 1,
+            }
+        case 'SUBSTRACT':
+            return {
+                ...state,
+                stock : state.stock >= 10 ? state.stock = 10 : state.stock + 1,
+                goods : state.goods <= 0 ? state.goods = 0 : state.goods - 1,
+            }
+        default:
+            return state
+    }
+}
+
+export default goodsReducer
