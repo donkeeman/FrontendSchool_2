f1dc7181cece92d2c88a3e4eaeb1d4bf4f672094
donkeeman <doonkeeemaan@gmail.com>
2022-07-01 20:54:01 +0900

    30일 실습

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+// const myLoader = require('./myLoader');
+const webpack = require('webpack');
+const childProcess = require('child_process');
 
 module.exports = {
     mode: 'development',
@@ -6,7 +9,42 @@ module.exports = {
         main: path.resolve('./src/app.js')
     },
     output: {
+        publicPath: '/webpack/dist/',
         filename: '[name].js',
         path: path.resolve('./dist')
-    }
+    },
+    module: {
+        rules: [
+            // {
+            //     test: /\.js$/,
+            //     use: [
+            //         path.resolve('./myLoader.js')
+            //     ]
+            // },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 200 * 1024
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.BannerPlugin({
+            banner: `
+                Commit version : ${childProcess.execSync('git rev-parse --short HEAD')}
+                Committer name : ${childProcess.execSync('git config user.name')}
+            `
+        })
+    ]
 }
